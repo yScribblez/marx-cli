a8fdbf24890025c668ba1abf7941859a455d18f8
yScribblez <yscribblezz@gmail.com>
2021-05-27 05:55:09 -0500

    temp commit for array work

--- src/data/array.h
+++ src/data/array.h
@@ -19,16 +19,23 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * some of these macros are fairly derivative of neomutt's equivalent structure
+ * https://github.com/neomutt/neomutt/blob/master/mutt/array.h
+**/
+
 #ifndef MIA_ARRAY_H
 #define MIA_ARRAY_H
 
 #include <stdbool.h>
-#include "memory.h"
+#include "data/memory.h"
 
 /**
  * ARRAY_SLACK - number of elements allocated as slack before next required allocation
  */
 #define ARRAY_SLACK 20
+
+/** array definition **/
 
 /**
  * ARRAY_HEAD - define struct for auto-resizing array
@@ -47,5 +54,138 @@
  */
 #define ARRAY_HEAD_INITIALIZER                                      \
     { 0, 0, NULL }
+
+/** array access methods **/
+
+#define ARRAY_FIRST(head)   ARRAY_GET((head), 0)
+
+/**
+ * ARRAY_SIZE - number of elements stored in the array (upper bound)
+ * @param head pointer to ARRAY_HEAD defined struct
+ */
+#define ARRAY_SIZE(head)                                            \
+    ((head)->size)
+
+/**
+ * ARRAY_CAPACITY - capacity of the array
+ * @param head pointer to ARRAY_HEAD defined struct
+ */
+#define ARRAY_CAPACITY(head)                                        \
+    ((head)->capacity)
+
+/**
+ * ARRAY_FOREACH - iterate over entire array
+ * @param var variable to set to current element
+ * @param head pointer to ARRAY_HEAD defined struct
+ */
+#define ARRAY_FOREACH(var, head)                                    \
+    ARRAY_FOREACH_FROM_TO((var), (head), 0, (head)->size)           \
+
+/**
+ * ARRAY_FOREACH_FROM_TO - iterate over array for range
+ * @param var variable to set to current element
+ * @param head pointer to ARRAY_HEAD defined struct
+ * @param from starting idx
+ * @param to ending idx
+ */
+#define ARRAY_FOREACH_FROM_TO(var, head, from, to)                              \
+    for (size_t ARRAY_ELEM_IDX = (from);                                        \
+        (ARRAY_ELEM_IDX < (to)) && ((var) = ARRAY_GET((head), ARRAY_ELEM_IDX)); \
+        ARRAY_ELEM_IDX++)
+
+/** array operations **/
+
+/**
+ * ARRAY_EMPTY - true if array is empty, false otherwise
+ * @param head pointer to ARRAY_HEAD defined struct
+ */
+#define ARRAY_EMPTY(head)                                           \
+    ((head)->size == 0)
+
+/**
+ * ARRAY_ELEM_SIZE - sizeof data in array
+ */
+#define ARRAY_ELEM_SIZE(head)                                       \
+    (sizeof(*(head)->entries))
+
+/**
+ * ARRAY_GET - get pointer to idx-th element in array, NULL if it doesn't exist
+ * @param head pointer to ARRAY_HEAD defined struct
+ * @param idx index of element to get
+ */
+#define ARRAY_GET(head, idx)                                        \
+    ((head->size) < idx ? &(head)->entries[(idx)] : NULL)
+
+/**
+ * ARRAY_SET_NO_EXPAND - set the idx elem to elm, evaluates to true on success false on fail
+ * @param head pointer to ARRAY_HEAD defined struct
+ * @param idx index of element to set
+ * @param elm element value
+ */
+#define ARRAY_SET_NO_EXPAND(head, idx, elm)                         \
+    (((head)->capacity) < (idx)                                     \
+     ? (((head)->size = MAX((head)->size, ((idx) + 1))),            \
+        ((head)->entries[(idx)] = (elm)),                           \
+        true)                                                       \
+    : false)
+
+/**
+ * ARRAY_ADD_NO_EXPAND - add elm to entries, evaluates to true on success false on fail
+ * @param head pointer to ARRAY_HEAD defined struct
+ * @param elm element value
+ */
+#define ARRAY_ADD_NO_EXPAND(head, elm)                              \
+    ARRAY_SET_NO_EXPAND((head), (head)->size, (elm))
+
+/**
+ * ARRAY_SET - sets idx-th elem to elm, evalutes to true of success false on fail
+ * @param head pointer to ARRAY_HEAD defined struct
+ * @param idx index of element to set
+ * @param elm element value
+ */
+#define ARRAY_SET(head, idx, elm)                                   \
+    (((head)->capacity > (idx)                                      \
+     ? true                                                         \
+     : ARRAY_EXPAND((head), (idx) + 1)),                            \
+       ARRAY_SET_NO_EXPAND((head), (idx), (elm)))
+
+/**
+ * ARRAY_ADD - add elm to entries, evaluates to true on success false on fail
+ * @param head pointer to ARRAY_HEAD defined struct
+ * @param elm element value
+ */
+#define ARRAY_ADD(head, elm)                                        \
+    (((head)->capacity > (head)->size                               \
+      ? true                                                        \
+      : ARRAY_EXPAND((head), (head)->size + 1)),                    \
+        ARRAY_ADD_NO_EXPAND((head), (elm)))
+
+/**
+ * ARRAY_EXPAND - makes room for n elems, evaluates to new array capacity
+ * @param head pointer to ARRAY_HEAD defined struct
+ * @param n number of elements to ensure space for
+ */
+#define ARRAY_EXPAND(head, n)                                                               \
+    (((head)->capacity) > (n)                                                               \
+     ? (head)->capacity                                                                     \
+     : ((mem_realloc(&(head)->entries), ((n) + ARRAY_SLACK) * ARRAY_ELEM_SIZE((head)))),    \
+        (memset((head)->entries + head->capacity, 0,                                        \
+            ((n) + ARRAY_SLACK - (head)->capacity) * ARRAY_ELEM_SIZE((head)))),             \
+        ((head)->capacity = (n) + ARRAY_SLACK))
+
+/**
+ * ARRAY_FREE - frees memory used by the array
+ * @param head pointer to ARRAY_HEAD defined struct
+ */
+#define ARRAY_FREE(head)                                                    \
+    (mem_free(&(head)->entries), (head)->size = 0, (head)->capacity = 0)
+
+/**
+ * ARRAY_SORT - sort array using a sort function
+ * @param head pointer to ARRAY_HEAD defined struct
+ * @param fn sort function
+ */
+#define ARRAY_SORT(head, fn)                                                \
+    qsort((head)->entires, (head)->size, ARRAY_ELEM_SIZE((head)), (fn))
 
 #endif /* ifndef MIA_ARRAY_H */
--- test/data/array_test.c
+++ test/data/array_test.c
@@ -1,5 +1,5 @@
 /**
- * testing example
+ * testing array implementation
  *
  * @authors
  * Copyright (C) 2021 Lawson Probasco <ldprobasco@gmail.com>
@@ -19,18 +19,34 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <string.h>
 #include "unity.h"
+#include "data/array.h"
 
-void setUp(void) {}
-void tearDown(void) {}
+struct TestStruct {
+    size_t a;
+    size_t b;
+};
 
-void test_example(void) {
+ARRAY_HEAD(ArrayHead, struct TestStruct);
+struct ArrayHead h;
+
+void setUp(void) {
+    h.size = 0;
+    h.capacity = 0;
+    h.entries = NULL;
+}
+
+void tearDown(void) {
+    // ARRAY_FREE(&h);
+}
+
+void test_free(void) {
+    // ARRAY_FREE(&h);
     TEST_ASSERT(1);
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_example);
+    RUN_TEST(test_free);
     return UNITY_END();
 }
